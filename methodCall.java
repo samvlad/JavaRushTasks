f4614e9cbe31bbeada43f1d82eafbd1c7d1638f2
Vladyslav Samoilov <samvla2010@gmail.com>
2020-05-01 21:02:58 +0300

    first commit

---
+++ methodCall.java
@@ -0,0 +1,13 @@
+public class methodCall
+{
+    public static void main(String[] args) {
+        print4("I like to move it, move it.");
+    }
+
+    public static void print4(String s) {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+}
